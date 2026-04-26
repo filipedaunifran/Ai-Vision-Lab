67731ef411a4d9bcdc23619ce2e5b7ecb095eb9d
soueufilipejose <soueufilipejose@gmail.com>
2026-04-26 01:04:20 -0300

    Add models for gestures and emotions

--- assets/js/models.js
+++ assets/js/models.js
@@ -1,1 +1,17 @@
+const MODELS = {
+  gestures: {
+    title: "Gestos",
+    resultLabel: "Gesto detectado",
+    instructionTitle: "Tente mostrar o dedo do meio",
+    instructionText: "Mantenha a mão bem iluminada e centralizada na câmera.",
+    path: "https://teachablemachine.withgoogle.com/models/KKVX3Lts-/"
+  },
 
+  emotions: {
+    title: "Emoções",
+    resultLabel: "Emoção detectada",
+    instructionTitle: "Mostre uma emoção para a câmera",
+    instructionText: "Tente expressões como feliz, triste, bravo ou surpreso.",
+    path: "./models/emotions/"
+  }
+};
